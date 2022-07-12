3fc61470c950211899ffb71f07546999a7b68487
Vladimir Falcon <v.falcon@familie-redlich.de>
2022-07-12 15:54:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+Sitepackage for the project "File Metadata Overlay Aspect"
+==============================================================
+
+Add some explanation here.
---
+++ ext_emconf.php
@@ -0,0 +1,32 @@
+<?php
+
+/**
+ * Extension Manager/Repository config file for ext "file_metadata_overlay_aspect".
+ */
+$EM_CONF[$_EXTKEY] = [
+    'title' => 'File Metadata Overlay Aspect',
+    'description' => 'Extend FileMetadataOverlayAspect to fix core  translation problem  forge.typo3.org issue 93025',
+    'category' => 'templates',
+    'constraints' => [
+        'depends' => [
+            'typo3' => '10.4.0-10.4.99',
+            'fluid_styled_content' => '10.4.0-10.4.99',
+            'rte_ckeditor' => '10.4.0-10.4.99',
+        ],
+        'conflicts' => [
+        ],
+    ],
+    'autoload' => [
+        'psr-4' => [
+            'FamilieRedlich\\FileMetadataOverlayAspect\\' => 'Classes',
+        ],
+    ],
+    'state' => 'stable',
+    'uploadfolder' => 0,
+    'createDirs' => '',
+    'clearCacheOnLoad' => 1,
+    'author' => 'Vladimir Falcon Piva',
+    'author_email' => 'v.falcon@familie-redlich.de',
+    'author_company' => 'familie redlich',
+    'version' => '1.0.0',
+];
