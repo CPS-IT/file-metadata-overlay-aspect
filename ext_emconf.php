6d919b43fc5058db624e86527414002da1dd6866
Vladimir Falcon <v.falcon@familie-redlich.de>
2023-01-05 13:29:02 +0100

    Update composer and ext_emconf.php files to support  TYPO3 V-11.5.x

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,20 +1,14 @@
 <?php
-
-/**
- * Extension Manager/Repository config file for ext "file_metadata_overlay_aspect".
- */
 $EM_CONF[$_EXTKEY] = [
     'title' => 'File Metadata Overlay Aspect',
     'description' => 'Extend FileMetadataOverlayAspect to fix core  translation problem  forge.typo3.org issue 93025',
     'category' => 'templates',
     'constraints' => [
         'depends' => [
-            'typo3' => '10.4.0-10.4.99',
-            'fluid_styled_content' => '10.4.0-10.4.99',
-            'rte_ckeditor' => '10.4.0-10.4.99',
+            'typo3' => '9.5.0-11.99.99',
         ],
-        'conflicts' => [
-        ],
+        'conflicts' => [],
+        'suggests' => [],
     ],
     'autoload' => [
         'psr-4' => [
