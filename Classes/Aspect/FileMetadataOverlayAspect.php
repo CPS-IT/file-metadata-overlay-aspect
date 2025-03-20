015ba4c32c239b577d8d6fde033a0bb46943e127
Vladimir Falcón Piva <v.falcon@familie-redllich.de>
2025-03-20 14:22:34 +0100

    Refactor with php-cs fix

--- Classes/Aspect/FileMetadataOverlayAspect.php
+++ Classes/Aspect/FileMetadataOverlayAspect.php
@@ -11,7 +11,6 @@ declare(strict_types=1);
 
 namespace Fr\FileMetadataOverlayAspect\Aspect;
 
-use PDO;
 use Psr\Http\Message\ServerRequestInterface;
 use TYPO3\CMS\Core\Context\Context;
 use TYPO3\CMS\Core\Context\LanguageAspect;
@@ -92,15 +91,15 @@ final class FileMetadataOverlayAspect
         $record = $queryBuilder
             ->select('*')
             ->from('sys_file_metadata')->where($queryBuilder->expr()->eq(
-            'l10n_parent',
-            $queryBuilder->createNamedParameter($parentUid, \TYPO3\CMS\Core\Database\Connection::PARAM_INT)
-        ), $queryBuilder->expr()->eq(
-            'sys_language_uid',
-            $queryBuilder->createNamedParameter($languageUid, \TYPO3\CMS\Core\Database\Connection::PARAM_INT)
-        ), $queryBuilder->expr()->eq(
-            't3ver_wsid',
-            $queryBuilder->createNamedParameter(0, \TYPO3\CMS\Core\Database\Connection::PARAM_INT)
-        ))->executeQuery()
+                'l10n_parent',
+                $queryBuilder->createNamedParameter($parentUid, \TYPO3\CMS\Core\Database\Connection::PARAM_INT)
+            ), $queryBuilder->expr()->eq(
+                'sys_language_uid',
+                $queryBuilder->createNamedParameter($languageUid, \TYPO3\CMS\Core\Database\Connection::PARAM_INT)
+            ), $queryBuilder->expr()->eq(
+                't3ver_wsid',
+                $queryBuilder->createNamedParameter(0, \TYPO3\CMS\Core\Database\Connection::PARAM_INT)
+            ))->executeQuery()
             ->fetchAssociative();
 
         if ($record) {
